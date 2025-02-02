82250f70b158996cec517717c0543cfb697c04a4
iozimniy <Kitriss@gmail.com>
2025-02-02 09:22:54 +0300

    get statistic

--- stat/stats-server/src/main/java/ru/practicum/repository/EndpointHitRepository.java
+++ stat/stats-server/src/main/java/ru/practicum/repository/EndpointHitRepository.java
@@ -1,18 +1,26 @@
 package ru.practicum.repository;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Repository;
+import ru.practicum.dto.ReadEndpointHitDto;
 import ru.practicum.model.EndpointHit;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Repository
 public class EndpointHitRepository {
 
     private final JdbcTemplate jdbcTemplate;
+    private final RowMapper<ReadEndpointHitDto> mapper;
 
-    public EndpointHitRepository(JdbcTemplate jdbcTemplate) {
+    public EndpointHitRepository(JdbcTemplate jdbcTemplate, RowMapper<ReadEndpointHitDto> mapper) {
         this.jdbcTemplate = jdbcTemplate;
+        this.mapper = mapper;
     }
 
     public void save(EndpointHit endpointHit) {
@@ -23,5 +31,32 @@ public class EndpointHitRepository {
                 endpointHit.getUri(),
                 endpointHit.getIp(),
                 Timestamp.valueOf(endpointHit.getTimestamp()));
+    }
+
+    public Collection<ReadEndpointHitDto> get(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
+        StringBuilder sql = new StringBuilder();
+
+        if (unique) {
+            sql.append(" DISTINCT(ip) ");
+        }
+
+        sql.append("COUNT(id) as count, uri FROM endpoint_hit GROUP BY uri HEAVING timestamp BETWEEN ? AND ?");
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("start", start);
+        params.addValue("end", end);
+
+        if (uris != null) {
+            StringBuilder urisString = new StringBuilder(uris.getFirst());
+
+            for (int i = 1; i < uris.size(); i++) {
+                urisString.append(", ").append(uris.get(i));
+            }
+
+            params.addValue("uris", uris);
+            sql.append(" AND uri IN (").append(urisString).append(")");
+        }
+
+        return jdbcTemplate.query(sql.toString(), mapper, params);
     }
 }
--- stat/stats-server/src/main/java/ru/practicum/service/EndpointHitService.java
+++ stat/stats-server/src/main/java/ru/practicum/service/EndpointHitService.java
@@ -4,12 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.dto.CreateEndpointHitDto;
+import ru.practicum.dto.ReadEndpointHitDto;
 import ru.practicum.model.EndpointHit;
 import ru.practicum.repository.EndpointHitRepository;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 public class EndpointHitService {
 
     private final EndpointHitRepository endpointHitRepository;
@@ -30,5 +34,11 @@ public class EndpointHitService {
                 : LocalDateTime.now());
 
         endpointHitRepository.save(endpointHit);
+    }
+
+    public Collection<ReadEndpointHitDto> getHits(LocalDateTime start, LocalDateTime end,
+                                                                  List<String> uris, boolean unique) {
+
+    return endpointHitRepository.get(start, end, uris, unique);
     }
 }
