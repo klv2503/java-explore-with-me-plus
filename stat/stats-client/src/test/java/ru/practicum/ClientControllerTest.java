6336c6158bcd03341da0bd9d8ad2568891430880
Pavel <Akhenaton01@yandex.ru>
2025-02-03 19:45:31 +0300

    Client commit (#5)
    * Client commit
    
    * Client commit fix
    
    ---------
    
    Co-authored-by: Pavel Appolonov <app.pavel1998@gmail.com>

---
+++ stat/stats-client/src/test/java/ru/practicum/ClientControllerTest.java
@@ -0,0 +1,27 @@
+package ru.practicum;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.RestClient;
+import ru.practicum.controller.ClientController;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class ClientControllerTest {
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    private RestClient restClient;
+
+    @InjectMocks
+    private ClientController clientController;
+
+    @Test
+    void testHitEndpoint() {
+        clientController.hit("192.168.0.1", "/api/test");
+
+        verify(restClient).post();
+    }
+}
+
