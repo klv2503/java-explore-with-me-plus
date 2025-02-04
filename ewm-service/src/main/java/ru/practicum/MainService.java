44361115b58e9594ec83de4b4c18ace5aad14dbb
klv2503 <levkuras@yandex.kz>
2025-02-04 05:53:45 +0100

    Ветка main_svc.

--- ewm-service/src/main/java/ru/practicum/MainService.java
+++ ewm-service/src/main/java/ru/practicum/MainService.java
@@ -2,14 +2,10 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MainService {
     public static void main(String[] args) {
-        //ConfigurableApplicationContext context =
         SpringApplication.run(MainService.class, args);
-        //StatsClient statsClient = context.getBean(StatsClient.class);
-        //statsClient.hit(new ParamHitDto());
     }
 }
