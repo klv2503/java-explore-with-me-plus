19934e09b6db90f688f2505142770b90cf8135d2
Pavel <Akhenaton01@yandex.ru>
2025-02-08 17:40:37 +0300

    Adding new functionality and bug fixes (#12)
    * Adding new functionality and bug fixes
    
    * Adding new functionality and bug fixes
    
    ---------
    
    Co-authored-by: Pavel Appolonov <app.pavel1998@gmail.com>

--- ewm-service/src/main/java/ru/practicum/errors/ErrorHandler.java
+++ ewm-service/src/main/java/ru/practicum/errors/ErrorHandler.java
@@ -7,7 +7,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.io.PrintWriter;
@@ -19,78 +18,41 @@ public class ErrorHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ApiError> handlerConstraintViolationException(final ConstraintViolationException e) {
-
-        StackTraceElement sElem = e.getStackTrace()[0];
-        String className = sElem.getClassName();
-        String str = className.substring(className.lastIndexOf(".") + 1);
-        log.info("\nConstraintViolationException error - Class: {}; Method: {}; Line: {}; \nMessage: {}",
-                str, sElem.getMethodName(), sElem.getLineNumber(), e.getMessage());
-
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        e.printStackTrace(pw);
-        String stackTrace = sw.toString();
-
-        String statusStr = HttpStatus.BAD_REQUEST.value() + " "
-                + HttpStatus.BAD_REQUEST.getReasonPhrase().replace(" ", "_");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ApiError(statusStr, "Got incorrect pathVariable", e));
+        return buildErrorResponse(e, HttpStatus.BAD_REQUEST, "Got incorrect pathVariable");
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ApiError> handlerEntityNotFoundException(final EntityNotFoundException e) {
-
-        StackTraceElement sElem = e.getStackTrace()[0];
-        String className = sElem.getClassName();
-        String str = className.substring(className.lastIndexOf(".") + 1);
-        log.info("\nDataIntegrityViolationException error - Class: {}; Method: {}; Line: {}; \nMessage: {}",
-                str, sElem.getMethodName(), sElem.getLineNumber(), e.getMessage());
-
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        e.printStackTrace(pw);
-        String stackTrace = sw.toString();
-
-        String statusStr = HttpStatus.NOT_FOUND.value() + " "
-                + HttpStatus.NOT_FOUND.getReasonPhrase().replace(" ", "_");
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ApiError(statusStr, "The required object was not found.", e));
+        return buildErrorResponse(e, HttpStatus.NOT_FOUND, "The required object was not found.");
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<ApiError> handlerDataIntegrityViolationException(final DataIntegrityViolationException e) {
+        return buildErrorResponse(e, HttpStatus.CONFLICT, "Integrity constraint has been violated.");
+    }
 
-        StackTraceElement sElem = e.getStackTrace()[0];
-        String className = sElem.getClassName();
-        String str = className.substring(className.lastIndexOf(".") + 1);
-        log.info("\nDataIntegrityViolationException error - Class: {}; Method: {}; Line: {}; \nMessage: {}",
-                str, sElem.getMethodName(), sElem.getLineNumber(), e.getMessage());
-
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        e.printStackTrace(pw);
-        String stackTrace = sw.toString();
-
-        String statusStr = HttpStatus.CONFLICT.value() + " "
-                + HttpStatus.CONFLICT.getReasonPhrase().replace(" ", "_");
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(new ApiError(statusStr, "Integrity constraint has been violated.", e));
+    @ExceptionHandler(ForbiddenActionException.class)
+    public ResponseEntity<ApiError> handlerForbiddenActionException(final ForbiddenActionException e) {
+        return buildErrorResponse(e, HttpStatus.CONFLICT, "Action not allowed.");
     }
 
     @ExceptionHandler
-    @ResponseStatus
     public ResponseEntity<ApiError> handlerOtherException(final Exception e) {
-        log.error("\nGot 500 status Internal server error {}", e.getMessage(), e);
-
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        e.printStackTrace(pw);
-        String stackTrace = sw.toString();
-
-        String statusStr = HttpStatus.INTERNAL_SERVER_ERROR.value() + " "
-                + HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase().replace(" ", "_");
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ApiError(statusStr, "Got 500 status Internal server error", e));
+        return buildErrorResponse(e, HttpStatus.INTERNAL_SERVER_ERROR, "Got 500 status Internal server error");
     }
 
+    private ResponseEntity<ApiError> buildErrorResponse(Exception e, HttpStatus status, String message) {
+        StackTraceElement sElem = e.getStackTrace()[0];
+        String className = sElem.getClassName();
+        String str = className.substring(className.lastIndexOf(".") + 1);
+        log.error("\n{} error - Class: {}; Method: {}; Line: {}; \nMessage: {}",
+                status, str, sElem.getMethodName(), sElem.getLineNumber(), e.getMessage());
+
+        StringWriter sw = new StringWriter();
+        e.printStackTrace(new PrintWriter(sw));
+        String statusStr = status.value() + " " + status.getReasonPhrase().replace(" ", "_");
+
+        return ResponseEntity.status(status)
+                .body(new ApiError(statusStr, message, e));
+    }
 }
