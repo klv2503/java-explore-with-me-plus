53265fa6154a626b5a97401b41e21be02b641139
iozimniy <Kitriss@gmail.com>
2025-02-14 22:30:35 +0300

    fix style

--- ewm-service/src/main/java/ru/practicum/compilations/service/CompilationServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/compilations/service/CompilationServiceImpl.java
@@ -20,7 +20,6 @@ import ru.practicum.events.service.PublicEventsServiceImpl;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
