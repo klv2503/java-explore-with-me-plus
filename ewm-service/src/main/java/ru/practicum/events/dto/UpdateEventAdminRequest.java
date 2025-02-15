9ff789f3bddd6791e53cbdd1eb9073b48257b43e
iozimniy <53001520+iozimniy@users.noreply.github.com>
2025-02-15 20:33:10 +0300

    fix update event validation (#25)

--- ewm-service/src/main/java/ru/practicum/events/dto/UpdateEventAdminRequest.java
+++ ewm-service/src/main/java/ru/practicum/events/dto/UpdateEventAdminRequest.java
@@ -2,6 +2,8 @@ package ru.practicum.events.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,15 +21,19 @@ import java.time.LocalDateTime;
 public class UpdateEventAdminRequest {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
+    @Size(min = 20, max = 2000)
     private String annotation;
     private Integer category;
+    @Size(min = 20, max = 7000)
     private String description;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DateConfig.FORMAT)
+    @Future
     private LocalDateTime eventDate;
     private Location location;
     private Boolean paid;
     private Integer participantLimit;
     private Boolean requestModeration;
     private EventStateAction stateAction;
+    @Size(min = 3, max = 120)
     private String title;
 }
